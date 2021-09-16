132f6b41c4d5281e7e8a4c7678e5d1f7c5061a20
Crowdbotics <team@crowdbotics.com>
2021-09-16 16:51:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lingering_feather_30607Identifier",
+  url: "https://lingering_feather_30607Identifier.botics.co",
+  api: "https://lingering_feather_30607Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
